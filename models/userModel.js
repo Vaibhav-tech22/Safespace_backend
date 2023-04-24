44bbf31e4ee0d0a2be1ffa41e9ca0185a2933947
agrim19 <agrim19229@iiitd.ac.in>
2023-04-24 16:50:51 +0530

    fix mongo url

--- models/userModel.js
+++ models/userModel.js
@@ -1,38 +1,40 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'Please enter your name'],
+        required: [true, "Please enter your name"],
         trim: true,
-        maxLength: [30, 'Your name cannot exceed 30 characters']
+        maxLength: [30, "Your name cannot exceed 30 characters"],
     },
     email: {
         type: String,
-        required: [true, 'Please enter your email'],
+        required: [true, "Please enter your email"],
         trim: true,
-        unique: true
+        unique: true,
     },
     phoneNumber: {
         type: String,
-        required: [true, 'Please enter your phone number'],
+        required: [true, "Please enter your phone number"],
         trim: true,
-        maxLength: [11, 'Your phone number cannot exceed 11 characters']
+        maxLength: [11, "Your phone number cannot exceed 11 characters"],
     },
     verified: {
         type: Boolean,
         default: false,
-        required:true
+        required: true,
     },
 });
 
-console.log("hello", process.env.JWT_EXPIRE)
 userSchema.methods.getJWTToken = function () {
-    return jwt.sign({ id: this._id, number:this.phoneNumber }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRE,
-    });
-  };
+    return jwt.sign(
+        {id: this._id, number: this.phoneNumber},
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRE,
+        }
+    );
+};
 
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const dotenv = require('dotenv')
-const connectDataBase = require("./config/dataBase.js")
-
+const dotenv = require("dotenv");
+const connectDataBase = require("./config/dataBase.js");
 
 //Handling uncaught exception
 process.on("uncaughtException", (err) => {
     console.log(`Error: , ${err.message}`);
     console.log(`Shutting down server due to unhandled promise rejection`);
     process.exit(1);
-})
+});
 
 //config file
-dotenv.config({path:"config/config.env"});
-const app = require('./app')
+dotenv.config({path: "config/config.env"});
+const app = require("./app");
 
 //Calling the function to connect the database
 connectDataBase();
 
-const server = app.listen(process.env.PORT, ()=>{
-    console.log(`The server has started at http://localhost/${process.env.PORT}`);
-})
+const server = app.listen(process.env.PORT, () => {
+    console.log(
+        `The server has started at http://localhost:${process.env.PORT}`
+    );
+});
 
 process.on("unhandledRejection", (err) => {
     console.log(`Error: , ${err.message}`);
@@ -26,4 +27,4 @@ process.on("unhandledRejection", (err) => {
     server.close(() => {
         process.exit(1);
     });
-})
+});
